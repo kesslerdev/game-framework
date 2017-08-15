2d7d53b78ca1cc528fb6ea4e171e3a7591fb157f
Kessler <kessler.dev@gmail.com>
2017-08-16 00:22:43 +0200

    add humanize

--- example/example-react/src/components/Infos/Resource/ResourceSlot.js
+++ example/example-react/src/components/Infos/Resource/ResourceSlot.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './ResourceSlot.css'
 
+import * as Humanize from 'humanize-plus'
+
 class ResourceSlot extends React.Component {
   render() {
     return (
-      <span className={'ResourceSlot' + (this.props.premium ? ' ResourceSlot-premium':'')}>{this.props.name}: {this.props.value}</span>
+      <span className={'ResourceSlot' + (this.props.premium ? ' ResourceSlot-premium':'')}>
+        {this.props.name}: {Humanize.intComma(this.props.value)}
+      </span>
     );
   }
 }
