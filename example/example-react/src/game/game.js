cdde5b8a9222b1c6c77873e40e51ef42d0104baa
Kessler <kessler.dev@gmail.com>
2017-09-10 23:20:02 +0200

    Add more shops in example advantage => only game files updated

--- example/example-react/src/game/game.js
+++ example/example-react/src/game/game.js
@@ -18,23 +18,49 @@ const Resources = {
 
 function getLemonStand() {
   const lemonStand = ShopTemplate.createGameObject('Lemon_Stand')
-  lemonStand.addCostSlot(Resources.Cash, 1)
+  lemonStand.addCostSlot(Resources.Cash, 4)
   // with upgrade lvl 2 double earnings
   // upgrade price 4 for 2 4.28 for 3, 4.58 for 4, 4.90,5.24,5.61
-  lemonStand.ProductionBaseTime = 3000
-  lemonStand.addProductionSlot(Resources.Cash, 4)
+  lemonStand.addProductionSlot(Resources.Cash, 1)//x level
+  lemonStand.ProductionBaseTime = 1000
 
   return lemonStand
 }
 
 function getNewsPaperDelivery(){
   const newsPaperDelivery = ShopTemplate.createGameObject('News_Paper_Delivery')
-  newsPaperDelivery.addCostSlot(Resources.Cash, 10)
-  newsPaperDelivery.addProductionSlot(Resources.Cash, 9)
-  newsPaperDelivery.ProductionBaseTime = 10000
+  newsPaperDelivery.addCostSlot(Resources.Cash, 60)
+  newsPaperDelivery.addProductionSlot(Resources.Cash, 60)
+  newsPaperDelivery.ProductionBaseTime = 3000
 
   return newsPaperDelivery
+}
 
+function getCarWash(){
+  const carWash = ShopTemplate.createGameObject('Car_Wash')
+  carWash.addCostSlot(Resources.Cash,720)
+  carWash.addProductionSlot(Resources.Cash, 550)
+  carWash.ProductionBaseTime = 6000
+
+  return carWash
+}
+
+function getPizzaDelivery(){
+  const pizzaDelivery = ShopTemplate.createGameObject('Pizza_Delivery')
+  pizzaDelivery.addCostSlot(Resources.Cash, 8640)
+  pizzaDelivery.addProductionSlot(Resources.Cash, 4400)
+  pizzaDelivery.ProductionBaseTime = 12000
+
+  return pizzaDelivery
+}
+
+function getDonutShop(){
+  const donutShop = ShopTemplate.createGameObject('Donut_Shop')
+  donutShop.addCostSlot(Resources.Cash, 103680)
+  donutShop.addProductionSlot(Resources.Cash, 52000)
+  donutShop.ProductionBaseTime = 24000
+
+  return donutShop
 }
 
 function getAnciantFabric() {
@@ -49,6 +75,9 @@ function getAnciantFabric() {
 const Shops = {
   LemonStand: getLemonStand(),
   NewsPaperDelivery: getNewsPaperDelivery(),
+  CarWash: getCarWash(),
+  PizzaDelivery: getPizzaDelivery(),
+  DonutShop: getDonutShop(),
   AnciantFabric: getAnciantFabric(),
 }
 
@@ -60,6 +89,7 @@ export function createPlayer(name) {
       .addResourceSlot(Resources.Cash)
       .addResourceSlot(Resources.Gold)
 
+    Shops.LemonStand.purchaseFor(player)
     return player
   }
 
