9fdd5de3f22260cea577fae291805a0211a214fd
Kessler dev <kessler.dev@gmail.com>
2017-10-16 08:09:21 +0200

    clean imports & reindeint gameObject

--- packages/quarkit-core/src/gameObject.js
+++ packages/quarkit-core/src/gameObject.js
@@ -1,32 +1,36 @@
 import { Mixin, mix } from 'quarkit-mixin'
 import getGOReference from './utils/reference'
-import PropertyMixin from './mixins/property-mixin'
-import EmitterMixin from './mixins/emitter-mixin'
-import CacheMixin from './mixins/cache-mixin'
-import LoopEntryMixin from './mixins/loop-entry-mixin'
-import LoggableMixin from './mixins/loggable-mixin'
+import {
+  PropertyMixin,
+  EmitterMixin,
+  CacheMixin,
+  LoopEntryMixin,
+  LoggableMixin,
+} from './mixins'
 
-export const GameObjectMixin = Mixin((superclass) => class extends mix(superclass).with(EmitterMixin, LoggableMixin, LoopEntryMixin, CacheMixin, PropertyMixin) {
+export const GameObjectMixin
+  = Mixin((superclass) => class extends mix(superclass)
+    .with(EmitterMixin, LoggableMixin, LoopEntryMixin, CacheMixin, PropertyMixin) {
 
-  constructor(...args) {
+      constructor(...args) {
     // mixins should either 1) not define a constructor, 2) require a specific
     // constructor signature, or 3) pass along all arguments.
-    super(...args)
-    this.slug = args[0]
-    this.createLogger(`go#${this.slug}`)
-  }
+        super(...args)
+        this.slug = args[0]
+        this.createLogger(`go#${this.slug}`)
+      }
 
-  equals(obj) {
-    return obj.slug === this.slug && this.constructor.name === obj.constructor.name
-  }
+      equals(obj) {
+        return obj.slug === this.slug && this.constructor.name === obj.constructor.name
+      }
 
-  static createGameObject(...args) {
-    return new this(...args)
-  }
+      static createGameObject(...args) {
+        return new this(...args)
+      }
 
-  toString() {
-    return getGOReference(this)
-  }
+      toString() {
+        return getGOReference(this)
+      }
 
 })
 
---
+++ packages/quarkit-core/src/mixins/index.js
@@ -0,0 +1,6 @@
+
+export * from './cache-mixin'
+export * from './emitter-mixin'
+export * from './loggable-mixin'
+export * from './loop-entry-mixin'
+export * from './property-mixin'
